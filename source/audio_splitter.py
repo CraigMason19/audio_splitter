ecde5c92ae567b4c9477d5e32d6bfa22eb7d5ff7
SadHappy <craigjm19@gmail.com>
2026-03-28 10:38:41 +0000

    Moved file loading into a reuseable function

--- source/audio_splitter.py
+++ source/audio_splitter.py
@@ -1,24 +1,29 @@
 import os
 
 from pathlib import Path
-from datetime import timedelta
 from pydub import AudioSegment
 
 from source.util import ms_to_hours_minutes
 from source.audio_config import audio_config
 from source.timestamp import Timestamp
 
-def split_into_chunks(filename, chunk_duration):
-    """
-    Splits a long .mp3 into smaller chunks
-    """
 
+def load_audio(filename: str):
     if not Path(filename).exists():
         raise FileNotFoundError(f"File '{filename}' not found.")
-
+    
     print(f"Loading file: {filename}")
     audio = AudioSegment.from_mp3(filename)
     print(f"Loaded {filename} successfully. Duration: {ms_to_hours_minutes(len(audio))}")
+
+    return audio
+    
+
+def split_into_chunks(filename: str, chunk_duration):
+    """
+    Splits a long .mp3 into smaller chunks
+    """
+    audio = load_audio(filename)
 
     segment_length = chunk_duration * 60 * 1000
 
@@ -32,21 +37,14 @@ def split_into_chunks(filename, chunk_duration):
     print("Splitting complete!")
 
 
-def export_section(filename, start_time: Timestamp, end_time: Timestamp) -> None:
+def export_section(filename: str, start_time: Timestamp, end_time: Timestamp) -> None:
     """
     Extracts a section of an audio file and exports it.
     """
-    if not Path(filename).exists():
-        raise FileNotFoundError(f"File '{filename}' not found.")
-    
-    print(f"Loading file: {filename}")
-    audio = AudioSegment.from_mp3(filename)
-    print(f"Loaded {filename} successfully. Duration: {ms_to_hours_minutes(len(audio))}")
+    audio = load_audio(filename)
 
-    print(f"Extracting section from {start_time} to {end_time}")
+    print(f"Extracting section from {start_time} to {end_time}...")
 
-
-    print("Splitting...")
     section = audio[int(start_time.to_timedelta().total_seconds() * 1000) : int(end_time.to_timedelta().total_seconds() * 1000)]
 
     # Generate output filename
