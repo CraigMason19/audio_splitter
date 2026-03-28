6bdb757f4a1750a02c89afdb6813f87588f39ddc
SadHappy <craigjm19@gmail.com>
2026-03-28 12:08:42 +0000

    Added 3 examples to main script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from source.audio_config import audio_config
-from source.audio_splitter import split_into_chunks, export_section
+from source.audio_splitter import split_into_chunks, split_on_timestamps, export_section
 from source.timestamp import Timestamp
 from source.util import configure_ffmpeg
 
@@ -12,10 +12,22 @@ if __name__ == "__main__":
     filename = os.path.join(audio_config.input_dir, "example_podcast.mp3")
 
 
+    # Split into chunks
     # split_length = 20
     # split_into_chunks(filename, split_length)   
 
 
-    start_time = Timestamp(0, 30, 0)
-    end_time = Timestamp(1, 0, 0)
-    export_section(filename, start_time, end_time)
+    # Extract section
+    # start_time = Timestamp(0, 30, 0)
+    # end_time = Timestamp(1, 0, 0)
+    # export_section(filename, start_time, end_time)
+
+
+    # Split on timestamps
+    timestamps = [
+        "0:00",
+        "17:00",
+        "21:00",
+    ]
+
+    split_on_timestamps(filename, timestamps)
