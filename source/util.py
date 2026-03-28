b7d71c6f947b23f8441405ef0caaee7142211691
SadHappy <craigjm19@gmail.com>
2026-03-28 11:23:44 +0000

    renamed ms_to_hours_minutes to ms_to_hms_str. Added ms_to_timestamp

--- source/util.py
+++ source/util.py
@@ -4,12 +4,22 @@ from datetime import timedelta
 from pydub import AudioSegment
 
 from source.audio_config import audio_config
+from source.timestamp import Timestamp
 
-def ms_to_hours_minutes(milliseconds):
+  
+def ms_to_hms_str(milliseconds) -> str:
     td = timedelta(milliseconds=milliseconds)
     hours, remainder = divmod(td.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
+
     return f"{hours} hours, {minutes} minutes, {seconds} seconds"
+
+def ms_to_timestamp(milliseconds) -> str:
+    td = timedelta(milliseconds=milliseconds)
+    hours, remainder = divmod(td.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    return Timestamp(hours, minutes, seconds)
 
 def configure_ffmpeg():
     AudioSegment.converter = os.path.join(audio_config.ffmpeg_path, "ffmpeg.exe")
